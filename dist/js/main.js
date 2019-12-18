e40a090c88697cd701fae3b20e7bd6e86746f00e
Kelly <kjk224@cornell.edu>
2019-12-18 13:42:06 -0800

    animations

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,40 @@
 
-$(document).ready(function() {
+$(document).ready(function () {
     $(".menu-nav").on("click", function () {
         $(this).toggleClass("open");
         $(".top-nav").toggleClass("open");
     })
+
+    $(".top-nav .navigation-link").on("click", function () {
+        $('.menu-nav').removeClass("open");
+        $('.top-nav').removeClass("open");
+    })
+
+    $('nav a[href*="#"]').on("click", function () {
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100 }, 2000);
+    });
+
+    
+
+
 });
+
+
+
+
+// $('body').scrollTo('#target'); // Scroll screen to target element
+
+// $('body').scrollTo(500); // Scroll screen 500 pixels down
+
+// $('#scrollable').scrollTo(100); // Scroll individual element 100 pixels down
+// Options:
+
+// scrollTarget: A element, string, or number which indicates desired scroll position.
+
+//     offsetTop: A number that defines additional spacing above scroll target.
+
+//         duration: A string or number determining how long the animation will run.
+
+//             easing: A string indicating which easing function to use for the transition.
+
+//                 complete: A function to call once the animation is complete.
