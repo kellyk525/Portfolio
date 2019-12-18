0defe71db5e17690303c91474fe0c34c094606f1
Kelly <kjk224@cornell.edu>
2019-12-18 15:44:27 -0800

    navbar location to the side

--- dist/js/main.js
+++ dist/js/main.js
@@ -14,8 +14,15 @@ $(document).ready(function () {
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100 }, 2000);
     });
 
-    
+    $('#up').on('click', function () {
+        $('html, body').animate({ scrollTop: 0}, 2000);
+    })
 
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
 
 });
 
