d2e718c973eefa9f7bae21681248dcf8150eb08b
Kelly <kjk224@cornell.edu>
2019-12-20 23:49:51 -0800

    adding skills

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
     $('nav a[href*="#"]').on("click", function () {
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100 }, 2000);
-    });
+    }); 
 
     $('#up').on('click', function () {
         $('html, body').animate({ scrollTop: 0}, 2000);
